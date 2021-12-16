6d01c21bef3c96dca5c71b92287ca1e09565417f
William <williamhjang@gmail.com>
2021-12-16 15:28:30 -0500

    basics for DFS traversal in python

--- python_DSA.py
+++ python_DSA.py
@@ -32,7 +32,7 @@ c.right = f
 def bfs_traversal(root):
     queue = [root]
 
-    while len(queue) is not 0:
+    while len(queue) != 0:
         popped_node = queue.pop(0)
 
         print(popped_node.value)
@@ -45,4 +45,34 @@ def bfs_traversal(root):
 
     return
 
-bfs_traversal(a)
+# bfs_traversal(a)
+# Should print a, b, c, d, e, f
+
+
+# Depth first traversal - going to the lowest layer first - explores lowest leaf/child node before moving laterally
+# Utilizes a stack - LIFO
+
+def dfs_traversal(root):
+    stack = [root]
+
+    while len(stack) != 0:
+        popped_node = stack.pop(len(stack)-1)
+
+        # Action
+        print(popped_node.value)
+
+        # Right
+        if popped_node.right is not None:
+            stack.append(popped_node.right)
+
+        # Left
+        if popped_node.left is not None:
+            stack.append(popped_node.left)
+
+        # Whichever if statement goes last is guaranteed to be top of the stack (thus first one popped)
+        # If you want to traversal left first, make the .left if statement last and vice versa for traversing right
+
+    return
+
+
+dfs_traversal(a)
