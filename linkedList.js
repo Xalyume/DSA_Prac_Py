e9833de3e8aa5c7a6e9607bf1888f210aea393d6
William <williamhjang@gmail.com>
2021-12-17 14:49:58 -0500

    simple methods in Linked List

--- linkedList.js
+++ linkedList.js
@@ -16,5 +16,45 @@ class Node {
 		this.next = null;
 	}
 
-	
+
 }
+
+class LinkedList {
+	constructor() {
+		this.head = null;
+	}
+
+	append(val) {
+		// Edgecase for if the instatiated Linked List is currently empty
+		if (this.head === null) {
+			this.head = new Node(val)
+			return
+		}
+
+		// traverse through the Linked List to find the last node
+		let curr = this.head;
+		while (curr.next !== null) {
+			curr = curr.next;
+		}
+
+		curr.next = new Node(val);
+	}
+
+	traverse() {
+		let curr = this.head;
+		while (curr !== null) { // want to also hit the last node (tail node)
+			console.log(curr.val)
+			curr = curr.next;
+		}
+	}
+
+}
+
+const list = new LinkedList();
+list.append('a');
+list.append('b');
+list.append('c');
+list.append('d');
+list.append('e');
+console.log(list.head)
+console.log(list.traverse())
