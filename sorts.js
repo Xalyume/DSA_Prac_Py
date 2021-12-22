ffed8f271630b81322717d713cc76752025401d7
William <williamhjang@gmail.com>
2021-12-22 17:26:51 -0500

    additional comments

--- sorts.js
+++ sorts.js
@@ -70,8 +70,8 @@ Time Complexity = O(n**2)
 function insertionSort(array) {
     for (let i = 0; i < array.length; i++) { // iterates through the entirity of the array
         for (j = i; j > 0; j--) { // looks only at every element BEFORE the current iteration of i
-            if (array[j] < array[j-1]) { // if current j value is less than value at j-1
-                // swap
+            if (array[j] < array[j-1]) { // if current j value is less than value at j-1 (does not need to hit array[0] since it does not have a preceding value)
+                // swap the 2 values
                 const temp = array[j]
                 array[j] = array[j-1]
                 array[j-1] = temp
